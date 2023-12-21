121b58848480d774b8e472d7c0c2f9a10dd9237c
Matthew LaForest <matt.laforest@rollbar.com>
2023-12-21 17:36:44 -0500

    filter out shared libs only return apps

--- .github/scripts/find-changesets.js
+++ .github/scripts/find-changesets.js
@@ -3,6 +3,14 @@ const {execSync} = require('child_process');
 const output = JSON.parse(execSync('pnpm turbo run build --dry-run=json --since=main'))
 const apps = output.packages
     // Filter out the root of the monorepo it isn't really an app
-    .filter(app => app !== '//')
+    .filter((app) => {
+        if (app === '//') return false
+
+        const task = output.tasks.find(task => task.package === app)
+        if (!task) 
+            throw new Error(`Unable to locate package ${app}`)
+    
+        return task.directory.startsWith('apps/')
+    })
     .map((app) => ({app}))
 console.log(JSON.stringify(apps))
