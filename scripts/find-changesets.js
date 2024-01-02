35ad45f6fce2d634601869da8e9a1fe65c1efb40
Matthew LaForest <matt.laforest@rollbar.com>
2024-01-02 14:51:53 -0500

    better way of handling errors?

--- scripts/find-changesets.js
+++ scripts/find-changesets.js
@@ -2,7 +2,9 @@
 const {execSync} = require('child_process')
 
 const output = JSON.parse(execSync('pnpx turbo run build --dry-run=json --since=origin/main'))
-const apps = output.packages
+
+try {
+    const apps = output.packages
     // Filter out the root of the monorepo it isn't really an app
     .filter((app) => {
         if (app === '//') return false
@@ -17,6 +19,11 @@ const apps = output.packages
         const task = output.tasks.find((task) => task.package === app)
         return {app, directory: task.directory}
     })
-throw new Error('foo')
+    
+    throw new Error('foo')
 
-console.log(JSON.stringify(apps))
+    console.log(JSON.stringify(apps))
+} catch(e) {
+    console.error(e)
+    process.exit(1)
+}
